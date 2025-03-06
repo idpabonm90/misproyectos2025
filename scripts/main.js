5b7f09203a24e5d74d4032ac7ab51ad52202a81c
idpabonm90 <idpabonm@gmail.com>
2025-03-06 18:50:50 -0500

    diseño del menú de navegación final

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,19 @@ const sidebarToggle = document.querySelector('.sidebar-toggle');
 sidebarToggle.addEventListener('click', () => {
     sidebar.classList.toggle('active');
 });
+
+// Selecciona todos los ítems del menú
+const menuItems = document.querySelectorAll('.sidebar-menu a');
+
+// Función para manejar la selección de ítems
+menuItems.forEach(item => {
+    item.addEventListener('click', () => {
+        // Remueve la clase 'active' de todos los ítems
+        menuItems.forEach(menuItem => {
+            menuItem.classList.remove('active');
+        });
+
+        // Agrega la clase 'active' al ítem seleccionado
+        item.classList.add('active');
+    });
+});
